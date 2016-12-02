62db78df57eadeeb22bfed3969d7715f9d084e57
william-bishop <admin@the402.net>
2016-12-01 22:34:48 -0700

    day 2

---
+++ 2/1.js
@@ -0,0 +1,28 @@
+let nextMoveMap = {
+  1: { R: 2, D: 4 },
+  2: { R: 3, D: 5, L: 1 },
+  3: { D: 6, L: 2 },
+  4: { R: 5, D: 7, U: 1 },
+  5: { R: 6, D: 8, L: 4, U: 2 },
+  6: { D: 9, L: 5, U: 3 },
+  7: { R: 8, U: 4 },
+  8: { R: 9, L: 7, U: 5 },
+  9: { L: 8, U: 6 }
+};
+
+function inputCode (instr) {
+  let code = ''
+  let key = 5;
+  instr.split('\n').forEach(line => {
+    [].forEach.call(line, ch => {
+      let nextMove = nextMoveMap[key][ch];
+      console.log(`moving ${ch} from ${key} to ${nextMove || key}`);
+      key = nextMove || key;
+    });
+    console.log(`\t\t\tPressing${key}`);
+    code += key;
+  });
+  return code;
+}
+
+module.exports = inputCode;
--- test/index.js
+++ test/index.js
@@ -1,11 +1,51 @@
 let assert   = require('assert');
 
-validate(1, 2, 'R8, R4, R4, R8', 4);
+validate(
+  1, 2, 'R8, R4, R4, R8', 4
+);
+
+validate(
+  2, 1, `ULL
+RRDDD
+LURDL
+UUUUD`, 1985
+);
+validate(
+  2, 1, `UUUUU
+LLLLLLLL
+DDRR
+LURUL`, 2192);
+validate(2,1,
+`L
+U
+DD
+UR
+U
+L
+RR
+LDR
+UDD
+ULD
+L`, 41752136987);
+validate(2, 1,
+`UUUUU
+LLLLLLUUUULLL
+DLLLL
+DLLLLLLDDDDDLLLL
+RDDD
+RDDDDRRRDDDD
+URRRRR
+URRRRUUUURRR
+LUUUUU`, 214789632);
 
 function validate (day, part, input, expectation) {
-  describe(`Day #${day}, Pt ${part}`, function () {
-    it(`should have correct output for: ${input}`, function () {
-      assert.equal(require(`../${day}/${part}`)(input), expectation);
+  let desc    = `Day #${day}, Pt ${part}`;
+  let should  = `${input} -> ${expectation}`;
+
+  describe(desc, function () {
+    it(should, function () {
+      let program = require(`../${day}/${part}`);
+      assert.equal(program(input), expectation);
     });
   });
 }
