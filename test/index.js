07295b1dea1632dd549827892392453e39976e54
william-bishop <admin@the402.net>
2016-12-01 23:00:11 -0700

    formatting

--- test/index.js
+++ test/index.js
@@ -4,17 +4,19 @@ validate(
   1, 2, 'R8, R4, R4, R8', 4
 );
 
-validate(
-  2, 1, `ULL
+validate(2, 1,
+`ULL
 RRDDD
 LURDL
 UUUUD`, 1985
 );
-validate(
-  2, 1, `UUUUU
+
+validate(2, 1,
+`UUUUU
 LLLLLLLL
 DDRR
 LURUL`, 2192);
+
 validate(2,1,
 `L
 U
@@ -27,8 +29,8 @@ LDR
 UDD
 ULD
 L`, 41752136987);
-validate(2, 1,
-`UUUUU
+
+validate(2, 1, `UUUUU
 LLLLLLUUUULLL
 DLLLL
 DLLLLLLDDDDDLLLL
