48e3216673d816dbc1078e826efcf320db8e5d13
william-bishop <admin@the402.net>
2016-12-03 01:11:45 -0700

    cleanup

--- 3/1.js
+++ 3/1.js
@@ -1,12 +1,8 @@
 function main(input) {
-  let tot = 0;
-
-  input.split('\n').forEach(line => {
+  return input.split('\n').reduce(function sumValidTriangles(tot, line) {
     let sides = line.trim().split(/\s+/).map(n => Number(n));
-    tot += Number(validTriangle.apply(null, sides));
-  });
-
-  return tot;
+    return tot += Number(validTriangle.apply(null, sides));
+  }, 0);
 }
 
 function validTriangle(a, b, c) {
