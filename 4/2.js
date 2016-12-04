7b02f70639e8cda90721ab3d04ca4f1ed8efaa2e
william-bishop <admin@the402.net>
2016-12-04 14:13:02 -0700

    returning secId

--- 4/2.js
+++ 4/2.js
@@ -2,14 +2,19 @@ let _ = require('lodash');
 let alph = _.range(97, 123).map(n => String.fromCharCode(n));
 
 function main (input) {
-  _(input).trim().split(/\n/).forEach((line, i) => {
-    let [ encName, secId, checkSum ] = line.match(/(.+)-(\d+)\[(.+?)]/).slice(1);
-    let decName = _(encName)
-      .map(ch => ch == '-' ? ' ' : alph[(alph.indexOf(ch) + Number(secId)) % 26])
-      .join('');
+  let result;
 
-    if (/northpole/.test(decName)) console.log(`${secId}:\t${decName}`);
-  });
+  _(input).trim().split(/\n/)
+    .forEach((line, i) => {
+      let [ encName, secId, checkSum ] = line.match(/(.+)-(\d+)\[(.+?)]/).slice(1);
+      let decName = _(encName)
+        .map(ch => ch == '-' ? ' ' : alph[(alph.indexOf(ch) + Number(secId)) % 26])
+        .join('');
+
+      if (/northpole/.test(decName)) result = secId;
+    });
+
+  return result;
 }
 
 module.exports = main;
