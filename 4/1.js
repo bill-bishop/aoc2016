8f64d90b6ade53721e60b52906c6c2af344e4e53
william-bishop <admin@the402.net>
2016-12-03 23:15:14 -0700

    day 4

--- 4/1.js
+++ 4/1.js
@@ -1,13 +1,66 @@
 function main (input) {
-	let result = '';
-	
+	let result = 0;
+
 	input.trim().split('\n')
-    	.map(line => line.split(/\s+/))
     	.forEach((line, i) => {
-    		let [] = line;
+    		let [ _, encName, secId, checkSum ] = line.match(/(.+)-(\d+)\[(.+?)]/);
+
+    		if (fiveHighestChars(encName).join('') === checkSum) {
+    			result += Number(secId);
+    		}
 		});
 
     return result;
+}
+
+function fiveHighestChars (str) {
+	let fqMap = frequencyMap(str);
+	let result = [];
+
+	for (let i = 0; i < 5; i++) {
+		let ch = highestProp(fqMap);
+		result.push(ch);
+		delete fqMap[ch];
+	}
+
+	return result;
+}
+
+function frequencyMap (str) {
+	let map = {};
+
+	str.split('').forEach(ch => {
+		if (ch === '-') return;
+		map[ch] = map[ch] || 0;
+		map[ch] += 1;
+	});
+
+	return map;
+}
+
+function highestProp (o) {
+	let max = 0;
+	let lowestChar;
+	let result;
+
+	for (let x in o) {
+		if (!lowestChar) {
+			lowestChar = x;
+		}
+
+		if (o[x] > max) {
+			max = o[x];
+			lowestChar = x;
+			result = x;
+		}
+		else if (o[x] === max && x < lowestChar) {
+			max = o[x];
+			lowestChar = x;
+			result = x;
+		}
+	}
+
+	return result;
 }
 
 module.exports = main;
---
+++ 4/2.js
@@ -0,0 +1,31 @@
+function main (input) {
+	let result;
+
+	input.trim().split('\n')
+    	.forEach((line, i) => {
+    		let [ _, encName, secId, checkSum ] = line.match(/(.+)-(\d+)\[(.+?)]/);
+
+    		let decName = encName.split('')
+    			.map(ch => shiftChar(ch, secId)).join('');
+
+    		if (decName.indexOf('northpole') > -1) {
+    			result = secId;
+    		}
+		});
+
+    return result;
+}
+
+let alph = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+function shiftChar (ch, n) {
+	if (ch === '-') return ' ';
+	let i = (alph.indexOf(ch) + Number(n)) % alph.length;
+	return alph[i];
+}
+
+function charCode (str) {
+	return str.charCodeAt(0);
+}
+
+module.exports = main;
