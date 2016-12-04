d73560d92426c607083576d1ba5b8f0b1efc92ec
william-bishop <admin@the402.net>
2016-12-04 08:54:06 -0700

    whitespace

--- 4/1.js
+++ 4/1.js
@@ -1,66 +1,66 @@
 function main (input) {
-	let result = 0;
+  let result = 0;
 
-	input.trim().split('\n')
-		.forEach((line, i) => {
-			let [ _, encName, secId, checkSum ] = line.match(/(.+)-(\d+)\[(.+?)]/);
+  input.trim().split('\n')
+    .forEach((line, i) => {
+      let [ _, encName, secId, checkSum ] = line.match(/(.+)-(\d+)\[(.+?)]/);
 
-			if (fiveHighestChars(encName).join('') === checkSum) {
-				result += Number(secId);
-			}
-		});
+      if (fiveHighestChars(encName).join('') === checkSum) {
+        result += Number(secId);
+      }
+    });
 
-	return result;
+  return result;
 }
 
 function fiveHighestChars (str) {
-	let fqMap = frequencyMap(str);
-	let result = [];
+  let fqMap = frequencyMap(str);
+  let result = [];
 
-	for (let i = 0; i < 5; i++) {
-		let ch = highestProp(fqMap);
-		result.push(ch);
-		delete fqMap[ch];
-	}
+  for (let i = 0; i < 5; i++) {
+    let ch = highestProp(fqMap);
+    result.push(ch);
+    delete fqMap[ch];
+  }
 
-	return result;
+  return result;
 }
 
 function frequencyMap (str) {
-	let map = {};
+  let map = {};
 
-	str.split('').forEach(ch => {
-		if (ch === '-') return;
-		map[ch] = map[ch] || 0;
-		map[ch] += 1;
-	});
+  str.split('').forEach(ch => {
+    if (ch === '-') return;
+    map[ch] = map[ch] || 0;
+    map[ch] += 1;
+  });
 
-	return map;
+  return map;
 }
 
 function highestProp (o) {
-	let max = 0;
-	let lowestChar;
-	let result;
+  let max = 0;
+  let lowestChar;
+  let result;
 
-	for (let x in o) {
-		if (!lowestChar) {
-			lowestChar = x;
-		}
+  for (let x in o) {
+    if (!lowestChar) {
+      lowestChar = x;
+    }
 
-		if (o[x] > max) {
-			max = o[x];
-			lowestChar = x;
-			result = x;
-		}
-		else if (o[x] === max && x < lowestChar) {
-			max = o[x];
-			lowestChar = x;
-			result = x;
-		}
-	}
+    if (o[x] > max) {
+      max = o[x];
+      lowestChar = x;
+      result = x;
+    }
+    else if (o[x] === max && x < lowestChar) {
+      max = o[x];
+      lowestChar = x;
+      result = x;
+    }
+  }
 
-	return result;
+  return result;
 }
 
 module.exports = main;
--- 4/2.js
+++ 4/2.js
@@ -1,27 +1,27 @@
 function main (input) {
-	let result;
+  let result;
 
-	input.trim().split('\n')
-		.forEach((line, i) => {
-			let [ _, encName, secId, checkSum ] = line.match(/(.+)-(\d+)\[(.+?)]/);
+  input.trim().split('\n')
+    .forEach((line, i) => {
+      let [ _, encName, secId, checkSum ] = line.match(/(.+)-(\d+)\[(.+?)]/);
 
-			let decName = encName.split('')
-				.map(ch => shiftChar(ch, secId)).join('');
+      let decName = encName.split('')
+        .map(ch => shiftChar(ch, secId)).join('');
 
-			if (/north ?pole/i.test(decName)) {
-				result = secId;
-			}
-		});
+      if (/north ?pole/i.test(decName)) {
+        result = secId;
+      }
+    });
 
-	return result;
+  return result;
 }
 
 let alph = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 function shiftChar (ch, n) {
-	if (ch === '-') return ' ';
-	let i = (alph.indexOf(ch) + Number(n)) % alph.length;
-	return alph[i];
+  if (ch === '-') return ' ';
+  let i = (alph.indexOf(ch) + Number(n)) % alph.length;
+  return alph[i];
 }
 
 module.exports = main;
