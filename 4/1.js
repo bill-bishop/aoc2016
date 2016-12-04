5eed41deb947708ce6fd4d664c17b2f155390556
william-bishop <admin@the402.net>
2016-12-03 21:59:14 -0700

    4/1 skeleton

---
+++ 4/1.js
@@ -0,0 +1,13 @@
+function main (input) {
+	let result = '';
+	
+	input.trim().split('\n')
+    	.map(line => line.split(/\s+/))
+    	.forEach((line, i) => {
+    		let [] = line;
+		});
+
+    return result;
+}
+
+module.exports = main;
